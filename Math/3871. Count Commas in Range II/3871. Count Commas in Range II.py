6ab0918b2c0e4d37ab660a4e29d2be5aa61e0cd1
Mithun A <mr.mithun2521@gmail.com>
2026-09-09 09:20:06 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.7%) - LeetHub

---
+++ Math/3871. Count Commas in Range II/3871. Count Commas in Range II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countCommas(self, n):
+        if n <= 999:
+            return 0
+
+        total = 0
+        start = 1000
+
+        while start <= n:
+            total += n - start + 1
+            start *= 1000
+
+        return total
